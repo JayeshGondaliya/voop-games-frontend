5b8182ed96de9a3676e1ee082e379f012467c9f4
Jayesh Gondaliya <jayeshgondaliya@gmail.com>
2025-06-21 18:00:06 +0530

    ok

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,14 +3,14 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Footer = () => {
     return (
-        <footer className="text-white p-4 ">
+        <footer className="text-white p-2 ">
             <div className="fluid-container mx-auto max-w-7xl">
 
                 {/* Upper Part */}
                 <div className="flex flex-col md:flex-row md:justify-between md:space-x-8 items-center md:items-start text-center sm:text-wrap">
 
                     {/* Description */}
-                    <div className="md:flex-1 max-w-md mx-auto md:mx-0 flex flex-col items-start mb-8 md:mb-0">
+                    <div className="md:flex-1 max-w-md mx-auto md:mx-0 flex flex-col items-start mb-2 md:mb-0">
                         <h3 className="text-base sm:text-lg md:text-xl font-semibold mb-4">About Toca Games World</h3>
                         <p className="leading-relaxed mb-4 text-left text-xs sm:text-sm md:text-base lg:text-lg">
                             Welcome to <span className="text-red-600 font-bold">Toca Games World</span>, your go-to destination for free online games! We offer a wide variety of HTML5 games that you can play instantly on any device—no downloads required. Our goal is to provide a fun, safe, and accessible gaming experience for players of all ages. Join us today and explore new games, challenge your friends, and dive into hours of endless entertainment!
@@ -19,10 +19,10 @@ const Footer = () => {
 
 
                     {/* Links + Social */}
-                    <div className=" flex flex-nowrap md:flex-wrap lg:flex-nowrap lg:justify-around gap-4 md:gap-8 md:justify-between text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl md:flex-1 max-w-3xl mx-auto">
+                    <div className="bg-red-500  md:w-1/2 flex flex-nowrap md:flex-wrap lg:flex-wrap text-center gap-4 md:gap-8 justify-between md:justify-between lg:justify-around text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl max-w-6xl mx-auto ">
 
                         {/* Our Links */}
-                        <div className="text-left w-1/2 md:w-auto">
+                        <div className="text-left w-1/2 md:w-auto text-center">
                             <h3 className="text-sm sm:text-base md:text-xl font-semibold mb-4">Our Links</h3>
                             <ul className="space-y-2">
                                 <li className="hover:text-indigo-400 cursor-pointer sm:text-base">Developers</li>
@@ -33,9 +33,9 @@ const Footer = () => {
                         </div>
 
                         {/* Social + Terms */}
-                        <div className="text-left w-1/2 md:w-auto">
+                        <div className="text-left w-1/2 md:w-auto flex flex-col text-center">
                             {/* Social Icons */}
-                            <div className="flex justify-center md:justify-start space-x-3 mb-4">
+                            <div className="flex justify-center md:justify-start space-x-3 mb-4 text-center">
                                 <div className="w-7 h-7 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-12 lg:h-12 rounded-full bg-indigo-600 hover:bg-indigo-700 transition flex items-center justify-center" title="Facebook">
                                     <i className="fab fa-facebook-f text-white text-xs sm:text-sm md:text-base"></i>
                                 </div>
@@ -51,19 +51,21 @@ const Footer = () => {
                             </div>
 
                             {/* Terms */}
-                            <div className="flex flex-col space-y-2 font-bold text-center text-base">
-                                <p className="hover:text-indigo-400 cursor-pointer">Terms of Use</p>
-                                <p className="hover:text-indigo-400 cursor-pointer">Privacy Policy</p>
+                            <div className="flex flex-col space-y-2 font-bold text-center sm:text-base">
+                                <p className="hover:text-indigo-400 cursor-pointer sm:text-base">Terms of Use</p>
+                                <p className="hover:text-indigo-400 cursor-pointer sm:text-base">Privacy Policy</p>
                             </div>
                         </div>
+
                     </div>
+
 
 
                 </div>
             </div>
 
             {/* Developed By */}
-            <div className="text-center text-white py-2 mt-8 text-sm">
+            <div className="text-center text-white  mt-8 text-sm">
                 <p>
                     <em className="not-italic font-light">Developed By </em>
                     <span className="font-semibold text-white">Jayesh K. Gondaliya, Ruchit D. Gevariya</span>
@@ -71,7 +73,7 @@ const Footer = () => {
             </div>
 
             {/* Copyright */}
-            <div className="text-center text-white py-2 text-sm">
+            <div className="text-center text-white  text-sm">
                 <p>
                     <span className="font-semibold text-white">© Voop Games - All Rights Reserved.</span>
                 </p>
